92e4bceedc386c6b4c250d29d2afbf26cb5aa7e1
NodirBobiev <nodir.bobiev@gmail.com>
2024-04-07 04:15:47 +0300

    initialize grpc stub once

--- client/serve.py
+++ client/serve.py
@@ -7,31 +7,34 @@ import random
 import pygame
 import sys
 
-def get_game_state():
+
+
+def get_game_state(stub):
     # Assuming the server is running on localhost and listening on port 50051
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = game_pb2_grpc.GameStub(channel)
-        response = stub.GetState(google.protobuf.empty_pb2.Empty())
-        # print("Game State Received:")
-        # # Example output of tanks and bullets, updated to follow Python naming conventions
-        # for tank in response.game_state.tanks:
-        #     print(f"Tank at ({tank.pos_x}, {tank.pos_y}) with health {tank.health}")
-        # for bullet in response.game_state.bullets:
-        #     print(f"Bullet at ({bullet.pos_x}, {bullet.pos_y}) with damage {bullet.damage}")
-        return response
+  
+    response = stub.GetState(google.protobuf.empty_pb2.Empty())
+    # print("Game State Received:")
+    # # Example output of tanks and bullets, updated to follow Python naming conventions
+    # for tank in response.game_state.tanks:
+    #     print(f"Tank at ({tank.pos_x}, {tank.pos_y}) with health {tank.health}")
+    # for bullet in response.game_state.bullets:
+    #     print(f"Bullet at ({bullet.pos_x}, {bullet.pos_y}) with damage {bullet.damage}")
+    return response
 
-def post_tank_event(event):
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = game_pb2_grpc.GameStub(channel)
-        
-        tank_id = "NULL"
-        request = game_pb2.PostTankEventRequest(tank_id=tank_id, event=event)
-        
-        # print(f"Posting event for tank {tank_id}: Event {event}")
-        stub.PostTankEvent(request)
+def post_tank_event(stub, event):
+    
+    tank_id = "NULL"
+    request = game_pb2.PostTankEventRequest(tank_id=tank_id, event=event)
+    
+    # print(f"Posting event for tank {tank_id}: Event {event}")
+    stub.PostTankEvent(request)
 
 
-def run():# Initialize Pygame
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = game_pb2_grpc.GameStub(channel) 
+
+    # Initialize Pygame
     pygame.init()
 
     # Set up the screen
@@ -66,10 +69,10 @@ def run():# Initialize Pygame
         keys = pygame.key.get_pressed()
         for key in events.keys():
             if keys[key]:
-                post_tank_event(events[key])
+                post_tank_event(stub, events[key])
         
         obj = []
-        test = get_game_state()
+        test = get_game_state(stub)
 
         for bullet in test.game_state.bullets:
             obj.append(parse_proto_to_bullet(bullet))
@@ -88,4 +91,5 @@ def run():# Initialize Pygame
 
     # Quit Pygame
     pygame.quit()
+    channel.close()
     sys.exit()
